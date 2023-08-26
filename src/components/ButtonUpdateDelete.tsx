73c12bdcb8c193ce787b56e58bd267874549b12e
Dexter Balderama <dhiee1598@gmail.com>
2023-08-26 08:27:50 +0800

    adding button update and delete components

---
+++ src/components/ButtonUpdateDelete.tsx
@@ -0,0 +1,47 @@
+'use client';
+
+import axios from 'axios';
+import { useRouter } from 'next/navigation';
+import Link from 'next/link';
+import { useState } from 'react';
+import { FaSpinner } from 'react-icons/fa';
+
+const ButtonUpdateDelete = ({ id }: { id: string }) => {
+  const [isBusy, setIsBusy] = useState<boolean>();
+  const router = useRouter();
+
+  const deleteBlog = async (value: string) => {
+    setIsBusy(true);
+
+    await axios
+      .delete(`http://localhost:3000/api/blogs/${value}`)
+      .then((res) => {
+        console.log(res.data);
+        setIsBusy(false);
+        router.push('/profile');
+      })
+      .catch((err) => {
+        setIsBusy(false);
+        console.error(err);
+      });
+  };
+
+  return (
+    <div>
+      <button
+        disabled={isBusy}
+        onClick={() => deleteBlog(id)}
+        className='w-auto text-center md:p-2 md:text-lg mr-1 bg-red-500 border border-black rounded-md p-1'
+      >
+        {isBusy ? <FaSpinner className='animate-spin m-auto' size={24} /> : 'Delete'}
+      </button>
+      <Link href={`/updateblog/${id}`}>
+        <button className='w-auto md:p-2 md:text-lg border bg-blue-500 border-black rounded-md p-1'>
+          Update
+        </button>
+      </Link>
+    </div>
+  );
+};
+
+export default ButtonUpdateDelete;
