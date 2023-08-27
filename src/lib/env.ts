119af6f66a7ef7dedc001a1742e0e8019f2b0d92
Dexter Balderama <dhiee1598@gmail.com>
2023-08-27 10:07:00 +0800

    zod

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -2,9 +2,9 @@ import zod from 'zod';
 
 const envSchema = zod.object({
   DATABASE_URL: zod.string().nonempty(),
-
+  DIRECT_URL: zod.string().nonempty(),
   NEXTAUTH_SECRET: zod.string().nonempty(),
-
+  NEXTAUTH_URL: zod.string().nonempty(),
   GOOGLE_CLIENT_ID: zod.string().nonempty(),
   GOOGLE_CLIENT_SECRET: zod.string().nonempty(),
 });
