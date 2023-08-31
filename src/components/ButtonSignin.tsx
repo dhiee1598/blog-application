799e0432b0e618d01c5b6a6edc338c44a88494cb
Dexter Balderama <dhiee1598@gmail.com>
2023-08-31 13:14:20 +0800

    update callback url

--- src/components/ButtonSignin.tsx
+++ src/components/ButtonSignin.tsx
@@ -6,7 +6,7 @@ const ButtonSignin = () => {
   return (
     <button
       className="text-base rounded px-3 py-1 mx-2 border border-purple-600 text-purple-600 hover:shadow-md hover:bg-purple-600 hover:text-white hover:border-transparent transition-colors duration-300"
-      onClick={() => signIn("google", { callbackUrl: "/" })}
+      onClick={() => signIn("google", { callbackUrl: "/api/auth/signin" })}
     >
       Login
     </button>
