98a458a5a4627d6bef7d538b03bb2934d70f68dc
Dexter Balderama <dhiee1598@gmail.com>
2023-08-31 14:00:33 +0800

    last na toh

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,32 +1,36 @@
-import Navbar from '@/components/Navbar';
-import './globals.css';
-import type { Metadata } from 'next';
-import { Poppins } from 'next/font/google';
-import Footer from '@/components/Footer';
-import QueryProvider from '@/components/QueryProvider';
-import SessionProviders from '@/components/SessionProviders';
-import { ThemeProvider } from 'next-themes';
-import ThemesProvider from '@/components/ThemesProvider';
+import Navbar from "@/components/Navbar";
+import "./globals.css";
+import type { Metadata } from "next";
+import { Poppins } from "next/font/google";
+import Footer from "@/components/Footer";
+import QueryProvider from "@/components/QueryProvider";
+import SessionProviders from "@/components/SessionProviders";
+import { ThemeProvider } from "next-themes";
+import ThemesProvider from "@/components/ThemesProvider";
 
 const poppins = Poppins({
-  subsets: ['latin'],
-  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
 });
 
 export const metadata: Metadata = {
-  title: 'Blog Mania',
-  description: 'My first project using NextJS 13.4',
+  title: "Blog Mania",
+  description: "My first project using NextJS 13.4",
 };
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
-    <html lang='en' className='light' style={{ colorScheme: 'light' }}>
+    <html lang="en" className="light" style={{ colorScheme: "light" }}>
       <body className={poppins.className}>
         <SessionProviders>
           <QueryProvider>
             <ThemesProvider>
               <Navbar />
-              {children}
+              <main className="max-w-5xl m-auto w-full p-4">{children}</main>
               <Footer />
             </ThemesProvider>
           </QueryProvider>
